a6e2b3efbc15bad594b2abbeaff50e1d2a66f3f0
adriel.santos <adriel200010@hotmail.com>
2022-07-22 12:44:16 -0300

    Definição do prefixo /api

--- start/routes.ts
+++ start/routes.ts
@@ -20,6 +20,9 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async () => {
-  return { hello: 'world' }
-})
+Route.group(() => {
+  Route.get('/', async () => {
+    return { hello: 'world' }
+  })
+  
+}).prefix('/api')
