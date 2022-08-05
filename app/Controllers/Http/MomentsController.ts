85ecdb00667c8fdff9354b28d462192ef309c295
adriel.santos <adriel200010@hotmail.com>
2022-08-04 23:32:39 -0300

    adicionar ibagens

--- app/Controllers/Http/MomentsController.ts
+++ app/Controllers/Http/MomentsController.ts
@@ -1,19 +1,41 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
+import {v4 as uuidv4} from 'uuid'
+
 import Moment from 'App/Models/Moment'
 
+import Application from '@ioc:Adonis/Core/Application'
+
 export default class MomentsController {
+    
+    private validationOptions = {
+        type: ['image'],
+        size: ['2mb']
+    }
+
     public async store({request, response}: HttpContextContract ) {
         
         const body = request.body()
         
-        const moment = await Moment.create(body);
+        const image = request.file('image', this.validationOptions)
+
+        if(image) {
+            const imageName = `${uuidv4()}.${image.extname}`
+
+            await image.move(Application.tmpPath('uploads'), {
+                name: imageName
+            })
+
+            body.image = imageName
+        }
+
+        const moment = await Moment.create(body)
 
         response.status(201)
     
         return{
             message: 'Momento criado com sucesso',
-            data: moment,
+            data: moment
         }
 
     }
